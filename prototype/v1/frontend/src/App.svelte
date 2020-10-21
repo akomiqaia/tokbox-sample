8138c3b932fb497d36094d413370179759c9f5bc
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-10-21 19:26:40 +0100

    add opentok in the app

--- prototype/v1/frontend/src/App.svelte
+++ prototype/v1/frontend/src/App.svelte
@@ -1,32 +1,17 @@
 <script>
-	export let name;
-	import Nested from './Nested.svelte'
+  import Nested from "./Nested.svelte";
+  import Video from "./Video.svelte";
+  let apiKey = "46958944";
+  let sessionId =
+    "1_MX40Njk1ODk0NH5-MTYwMzMwMzAwNTU2N35TUHYxbFhFVkU1MVlPbi9SRHlBY3Q3S1Z-fg";
+  let token =
+    "T1==cGFydG5lcl9pZD00Njk1ODk0NCZzaWc9NjE4YzliMmZjMzljNmQ1ZmQ2ODc0ZTY0MGZiMTc1ZTQxZGNhNmFmYjpzZXNzaW9uX2lkPTFfTVg0ME5qazFPRGswTkg1LU1UWXdNek13TXpBd05UVTJOMzVUVUhZeGJGaEZWa1UxTVZsUGJpOVNSSGxCWTNRM1MxWi1mZyZjcmVhdGVfdGltZT0xNjAzMzA0NTE4Jm5vbmNlPTAuODMwMDIzNDk5OTY1NjM3NyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjAzMzkwOTE4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Nested />
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<main>
+  <Nested />
+  <!-- <Video {apiKey} {sessionId} {token} /> -->
+</main>
